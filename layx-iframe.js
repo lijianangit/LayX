d8041d571cd51619c50a8b93611f7d1fc7f577c9
百小僧 <monk@baisoft.org>
2018-05-01 19:28:16 +0800

    v1.0 beta

---
+++ layx-iframe.js
@@ -0,0 +1,8 @@
+﻿window.onload = function () {
+    window.onclick = function () {
+        if (self != top && self.frameElement && self.frameElement.tagName == "IFRAME") {
+            var layxContainer = self.frameElement.parentNode.parentElement;
+            layxContainer.click();
+        }
+    }
+}
