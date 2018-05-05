b907e5d593d5d6fb6e11986bee6b700d1b79138e
百小僧 <monk@baisoft.org>
2018-05-05 19:40:10 +0800

    override all code

--- layx-iframe.js
+++
@@ -1,8 +0,0 @@
-﻿window.onload = function () {
-    window.onclick = function () {
-        if (self != top && self.frameElement && self.frameElement.tagName == "IFRAME") {
-            var layxContainer = self.frameElement.parentNode.parentElement;
-            layxContainer.click();
-        }
-    }
-}
