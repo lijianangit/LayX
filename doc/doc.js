ac9bc42d6c6df2e48e388c45eb5d66074c25ac45
百小僧 <monk@baisoft.org>
2018-05-27 19:17:55 +0800

    v2.2.5

--- doc/doc.js
+++ doc/doc.js
@@ -403,17 +403,15 @@ window.onload = function () {
             e = e || window.event;
             var scrollDiv = code.querySelector("*[name='" + this.innerHTML + "']");
             if (self != top && self.frameElement && self.frameElement.tagName == "IFRAME") {
-                if (navigator.userAgent.indexOf("Firefox") > 0) {
+                if (navigator.userAgent.indexOf("Firefox") > 0 || window.navigator.userAgent.toLowerCase().indexOf('iphone') > -1) {
                     (document.documentElement || document.body).scrollTop = scrollDiv.offsetTop - window.innerHeight - 50;
-                }
-                else {
                     if (window.navigator.userAgent.toLowerCase().indexOf('iphone') > -1) {
                         code.scrollTop = scrollDiv.offsetTop;
                     }
-                    else {
-                        (document.documentElement || document.body).scrollTop = scrollDiv.offsetTop;
-                        code.scrollTop = scrollDiv.offsetTop;
-                    }
+                }
+                else {
+                    (document.documentElement || document.body).scrollTop = scrollDiv.offsetTop;
+                    code.scrollTop = scrollDiv.offsetTop;
                 }
             }
             else {
