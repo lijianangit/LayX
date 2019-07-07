887a2b15fa2a24f7e1513b66d4bdd61906959c6a
百小僧 <monksoul@outlook.com>
2019-07-07 13:27:03 +0800

    update publicity slogans.

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
             canPrint: true
         }),
         new HtmlWebPackPlugin({
-            title: "入口即窗口，窗口即天地 - Layx",
+            title: "窗口即入口，入口即天地 - Layx",
             template: "./template.html",
             inject: "head",
             hash: true
