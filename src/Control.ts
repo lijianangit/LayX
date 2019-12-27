a8dc07b3f76a7214bbeb3ff8c52b06d051f8bd8f
百小僧 <monksoul@outlook.com>
2019-12-27 18:33:48 +0800

    1. 调整代码

--- src/Control.ts
+++ src/Control.ts
@@ -2,8 +2,10 @@
 import { isNumber, max, min, match, length } from "./app/core/validator/ValidatorDecorator";
 
 export default class Control {
+    // 现在的代码
     @length(3, 10)
     public name: string;
+
     constructor(theName: string) {
         this.name = theName;
     }
