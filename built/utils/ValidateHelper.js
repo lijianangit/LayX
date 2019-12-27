cf75de3815d65778e1000209c83e97192d84d1c1
百小僧 <monksoul@outlook.com>
2019-12-27 10:58:07 +0800

    Layx-Next 开始

--- built/utils/ValidateHelper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isStringAndNotEmptyOrNull(obj) {
-    return obj
-        && typeof obj === "string"
-        && obj.trim().length > 0;
-}
-exports.isStringAndNotEmptyOrNull = isStringAndNotEmptyOrNull;
--- src/asset/style/Index.ts
+++
@@ -1,3 +0,0 @@
-import "./common.css";
-import "./animate.css";
-import "./block.css";
