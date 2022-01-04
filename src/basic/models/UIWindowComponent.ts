8236db1ff2657f900bfd5de18435fb3fa23735a9
百小僧 <monksoul@outlook.com>
2022-01-04 14:55:29 +0800

    ok

--- src/basic/models/UIWindowComponent.ts
+++
@@ -1,9 +0,0 @@
-import App from "../../core/App";
-import UIComponent from "./UIComponent";
-import UIWindow from "../../controls/UIWindow";
-
-export default class UIWindowComponent extends UIComponent {
-    constructor(app: App, public readonly window: UIWindow) {
-        super(app);
-    }
-}
